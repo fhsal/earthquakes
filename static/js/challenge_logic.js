d38af8a8bc9233889a9ded69d31eb3ba91b66136
F.  Salvo <68975104+fhsal@users.noreply.github.com>
2023-07-18 14:05:33 -0400

    adding pai key

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -1,6 +1,10 @@
 // Add console.log to check to see if our code is working
 console.log("zzworking");
 
+
+const API_KEY='pk.eyJ1IjoiZmhzYWx2byIsImEiOiJjbDNocTRhZHgxNHMyM3FvNDB2YjZtNHJ5In0.jUQlA_xeJtGnR3S8CazpLQ'
+
+
 // We create the tile layer that will be the background of our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
